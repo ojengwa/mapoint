17360d47aaff4467a1717524c6661c74391edfc1
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-03-03 23:52:30 +0100

    update map views

--- assets/app/controllers/homeController.js
+++ assets/app/controllers/homeController.js
@@ -1,10 +1,28 @@
 app.controller("HomeController", [
-    "$scope",
     "$location",
     "$log",
     "uiGmapGoogleMapApi",
-    function ($scope, $location, $log, uiGmapGoogleMapApi) {
+    function ($location, $log, uiGmapGoogleMapApi) {
         'use strict';
+        this.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 10,
+            pan: true,
+            options: {
+                disableDefaultUI: true
+            },
+            events: {
+                click: get_place
+            }
+        };
 
+        // analyze the map click event
+        function get_place (map, e, args) {
+            // body...
+            $log.debug(map, e, args);
+        }
         $log.debug("Home Controller Initialized");
     }]);
